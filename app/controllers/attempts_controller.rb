34725ae30d361f800e48b8ad1cc039c1985c1cac
Yuin <yhuang1223@gmail.com>
2013-10-16 12:40:01 +0100

    percentage of correctly scored answers added

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -1,15 +1,18 @@
 class AttemptsController < ApplicationController
-  def new
-    @quiz = Quiz.find params[:quiz_id]
-  end
+	def new
+		@quiz = Quiz.find params[:quiz_id]
+	end
 
-  def create
-    @quiz = Quiz.find params[:quiz_id]
-    scorer = Scorer.new(@quiz)
-    render text: "#100/{scorer.total(params[:answer_ids].values)}%"
-  end
+	def create
+		@quiz = Quiz.find params[:quiz_id]
+		scorer = Scorer.new(@quiz)
+		guesses = params[:answer_ids].values
 
-def show
-  end
+		percent = ((scorer.total(guesses) / guesses.length.to_f) * 100).to_i
+		render text: "#{percent}%"
+	end
+
+	def show
+	end
 
 end
